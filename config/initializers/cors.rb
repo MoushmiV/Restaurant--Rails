9bf6024bbf2831953c92465b73572e6e810055da
Lena Hou <lenahou2013@gmail.com>
2022-01-26 17:09:54 -0500

    updating cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'http://172.23.235.231:3000',
+    origins '*',
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
